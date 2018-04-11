8d3e50900cf225274e37cd057844766f9489da75
Robbie Perry <robbieperry22@gmail.com>
2018-04-10 17:28:52 -0700

    Done

--- ImageChallenge/ViewController.swift
+++ ImageChallenge/ViewController.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePickerController = UIImagePickerController()
+    
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBAction func SelectButton(_ sender: Any) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected an image, but was provided (info)")
+        }
+        Image.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        if let (r,g,b,a) = pixel(in: selectedImage, at: CGPoint(x: 0, y:0)) {
+            self.view.backgroundColor = UIColor(displayP3Red: CGFloat(r)/255.0,
+                                                green: CGFloat(g)/255.0,
+                                                blue: CGFloat(b)/255.0,
+                                                alpha: CGFloat(a)/255.0)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
+            self.view.backgroundColor = selectedImage.averageColour()
+        }
+    }
+    
+    func pixel(in image: UIImage, at point: CGPoint) -> (UInt8, UInt8, UInt8, UInt8)? {
+        let width = Int(image.size.width)
+        let height = Int(image.size.height)
+        let x = Int(point.x)
+        let y = Int(point.y)
+        guard x < width && y < height else {
+            return nil
+        }
+        guard let cfData:CFData = image.cgImage?.dataProvider?.data, let pointer = CFDataGetBytePtr(cfData) else {
+            return nil
+        }
+        let bytesPerPixel = 4
+        let offset = (x + y * width) * bytesPerPixel
+        return (pointer[offset], pointer[offset + 1], pointer[offset + 2], pointer[offset + 3])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension UIImage {
+    func pixelData() -> [UInt8] {
+        let size = self.size
+        let dataSize = size.width * size.height * 4
+        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
+        return pixelData
+    }
+    func averageColour() -> UIColor? {
+        guard let inputImage = CIImage(image: self) else { return nil }
+        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+        
+        guard let filter = CIFilter(name: "CIAreaAverage", withInputParameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
+        guard let outputImage = filter.outputImage else { return nil }
+        
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [kCIContextWorkingColorSpace: kCFNull])
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: nil)
+        
+        return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
+    }
+}
